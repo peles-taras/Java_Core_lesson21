b5d291e023fd491c4f2fa26c53144ac0db44b326
user <user@WWW>
2022-01-19 01:59:42 +0200

    Changes 2

--- lesson-21/src/maximum/Methods.java
+++ lesson-21/src/maximum/Methods.java
@@ -12,16 +12,19 @@ public class Methods {
 
 	public void toLocalDate(Date date) {
 		LocalDate ld = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()).toLocalDate();
+		System.out.print("LocalDate: ");
 		System.out.println(ld);
 	}
 
 	public void toLocalTime(Date date) {
 		LocalTime lt = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()).toLocalTime();
+		System.out.print("LocalTime: ");
 		System.out.println(lt);
 	}
 
 	public void toLocalDateTime(Date date) {
 		LocalDateTime ldt = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+		System.out.print("LocalDateTime: ");
 		System.out.println(ldt);
 	}
 }
